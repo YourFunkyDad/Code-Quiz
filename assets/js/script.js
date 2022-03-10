0e7eaa21cf1f50e350ba3414b9ce7855e7f49068
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-03-10 11:52:51 -0500

    More Questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,31 +84,51 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: 'Is this working?',
+        question: 'Is coding fun?',
         answers: [
-            { text: 'It is working', correct: true },
-            { text: 'It is not working', correct: false }
+            { text: 'Yes', correct: true },
+            { text: 'Yes', correct: true },
+            { text: 'Yes', correct: true },
+            { text: 'Yes', correct: true }
+            
         ]  
       },  
     {
-        question: 'How about this one?',
+        question: 'Commonly used data types DO NOT Include:?',
         answers: [
-            { text: 'Still working', correct: true },
-            { text: 'You done broke the shit', correct: false },
-            { text: 'You really done fucked it up', correct: false },
-            { text: 'Nope', correct: false }
+            { text: 'Strings', correct: false },
+            { text: 'Booleans', correct: true },
+            { text: 'Alerts', correct: false },
+            { text: 'Numbers', correct: false }
         ]  
       },
       {
-        question: 'Is you dumb?',
+        question: 'String values must be enclosed within _______ when being assigned to variables.',
         answers: [
-            { text: 'Yes', correct: true },
-            { text: 'No', correct: false },
-            { text: 'Nope', correct: false },
-            { text: 'Not at all', correct: false }
+            { text: 'Commas', correct: false },
+            { text: 'Curly Brackets', correct: false },
+            { text: 'Quotes', correct: true },
+            { text: 'Parenthesis', correct: false }
         ]  
       },
-        
+      {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            { text: 'JavaScript', correct: false },
+            { text: 'Terminal/Bash', correct: false },
+            { text: 'For Loops', correct: false },
+            { text: 'console.log', correct: true }
+        ]  
+      },
+      {
+        question: 'Arrays in JavaScript can be used to store _______.',
+        answers: [
+            { text: 'Numbers and Strings', correct: false },
+            { text: 'Other Arrays', correct: false },
+            { text: 'Booleans', correct: false },
+            { text: 'All of the above', correct: true }
+        ]  
+      },  
 ]
 
 // Countdown Timer
